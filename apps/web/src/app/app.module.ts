7e7754fa4c095fc3cc6281bc74ec97377a23fcb9
Max Geller <mxgeller@gmail.com>
2020-05-11 19:01:23 -0700

    updated app module

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -1,14 +1,51 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+// import { CoreModule } from './core/core.module';
+// import { SharedModule } from './shared/shared.module';
+// import { AppRoutes } from './app.routes';
+// import { AuthModule } from './auth/auth.module';
+// import { AuthService } from './core/services/auth.service';
+// import { environment } from '../environments/environment.dev';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule,
+    HttpClientModule,
+    NgxSpinnerModule,
+    AngularFireModule,
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+
+
+  ],
+  providers: [
+    ,
+
+  ],
+  schemas: [],
+  bootstrap: [
+    AppComponent,
+  ],
 })
 export class AppModule {}
